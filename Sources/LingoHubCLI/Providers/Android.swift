a5a4e6cf5c43ada6399acc8929dfc2b9b74d7a55
Adam Eri <adam.eri@blackmirror.media>
2017-11-28 15:58:16 +0100

    Finalised Android version, updated readme.

--- Sources/LingoHubCLI/Providers/Android.swift
+++ Sources/LingoHubCLI/Providers/Android.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-
+import Alamofire
 
 open class Android: ResourceProvider {
 
@@ -27,39 +27,108 @@ open class Android: ResourceProvider {
 
     let fileManager = FileManager()
     var stringsFiles: [String] = []
+    let config = self.config
 
-//    // Strings files from the English base translation.
-//    // If `projectPath` is specified from there, if not, then the
-//    // current directory
-//    let projectPath = self.config.projectPath ??
-//      FileManager.default.currentDirectoryPath
-//
-//    // Reading all string files if folder path is specified
-//    if let stringsFolder = self.config.stringsFolder {
-//      let stringsFolderPath = projectPath + stringsFolder
-//      do {
-//        let allFilesInFolder = try fileManager
-//          .contentsOfDirectory(atPath: stringsFolderPath)
-//
-//        // Filtering only the strings files
-//        stringsFiles = allFilesInFolder
-//          .filter { $0.contains(".xml") }
-//          .map { stringsFolderPath + $0 }
-//      } catch {
-//        print(error)
-//      }
-//    }
-//
-//    for stringFile in self.config.stringsFiles {
-//      stringsFiles.append(projectPath + stringFile)
-//    }
+    // The config can specify:
+    // - an optional absolute `projectFolder`, defaults to the current folder.
+    // - an optional `translationFolder` within the `projectFolder`
+    // - a mandatory `baseLocale`, from which the `lproj` folder can be found
+    var stringsFolder = config.projectFolder ??
+      FileManager.default.currentDirectoryPath
 
+    // `translationFolder`
+    if config.translationFolder != nil {
+      stringsFolder += config.translationFolder!
+    }
+
+    // `baseLocale`
+    stringsFolder += "/values/"
+
+    // Reading all string files if folder path is specified
+    do {
+      let allFilesInFolder = try fileManager
+        .contentsOfDirectory(atPath: stringsFolder)
+
+      // Filtering only the strings files
+      stringsFiles = allFilesInFolder
+        .filter { $0.contains("strings.xml") }
+        .map { stringsFolder + $0 }
+    } catch {
+      print(error)
+    }
     return stringsFiles
   }
 
   public func save(
     resources: [LingoHubResource],
     completion: @escaping (() -> Void)) {
+    var downloadCount: Int = 0
 
+    for resource in resources {
+      guard let downloadUrl = resource.links.first?.href else {
+        print("No download url for resource: \(resource)")
+        continue
+      }
+
+      let locale = resource.locale
+      // We do not download the base locale.
+      guard locale != self.config.baseLocale else {
+        continue
+      }
+
+      // Removing locale from the resource name
+      // Localizable.de.strings -> Localizable.strings
+      let name = resource
+        .name
+        .replacingOccurrences(
+          of: "\(self.config.separator)\(locale)",
+          with: "",
+          options: String.CompareOptions.caseInsensitive,
+          range: nil)
+
+      // Making the download destination
+
+      // The config can specify:
+      // - an optional absolute `projectFolder`, defaults to the current folder.
+      // - an optional `translationFolder` within the `projectFolder`
+      // - a mandatory `baseLocale`, from which the `lproj` folder can be found
+      var destinationFolder = config.projectFolder ??
+        FileManager.default.currentDirectoryPath
+
+      // `translationFolder`
+      if config.translationFolder != nil {
+        destinationFolder += config.translationFolder!
+      }
+
+      // `locale` and name
+      destinationFolder += "/values-\(locale)/\(name)"
+
+      let destinationUrl = URL(fileURLWithPath: destinationFolder)
+
+      let destination: DownloadRequest.DownloadFileDestination = { _, _ in
+        return (destinationUrl, [
+          .removePreviousFile,
+          .createIntermediateDirectories])
+      }
+
+      downloadCount += 1
+
+      Alamofire
+        .download(
+          downloadUrl + "?auth_token=" + self.config.token,
+          to: destination)
+        .response { response in
+
+          if response.error == nil,
+            let filePath = response.destinationURL?.path {
+            print("Downloaded: ", filePath)
+          }
+
+          downloadCount -= 1
+          if downloadCount == 0 {
+            completion()
+          }
+      }
+    }
   }
 }
